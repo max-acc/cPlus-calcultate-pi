95e59aa8d1ad5c4182c4366dab78b03ab41db0e4
An Alien <max.wenk@protonmail.com>
2021-08-03 03:33:10 +0200

    Create calculatePi.cpp

---
+++ calculatePi.cpp
@@ -0,0 +1,23 @@
+//Calculating Pi until the eight digit
+//Term to calculate Pi (π^4)*90 = 1/(n^4) ... was developed by Euler and is a continued fraction
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+float intermediateResult;
+float pi;
+
+int main()
+{
+
+    for (int i = 1; i <= 50; i++) {     //The continued fration will be continued 50 times
+        intermediateResult += 1 / pow(i, 4);    //Calculating the continued fraction
+    }
+
+    intermediateResult *= 90;   //Transform expression
+    pi = sqrt(sqrt(intermediateResult));    //Getting the fourth root
+
+    cout << "Pi is equivalent to" << pi;    //Printing the result
+}
